68c0aa60cd14a8187d5e79ba0999612fb4564980
Eslam Ehab <aboutalebrap@gmail.com>
2020-03-15 16:11:25 +0200

    Add files via upload

--- HAL/DC_Motor/Motor.c
+++ HAL/DC_Motor/Motor.c
@@ -4,6 +4,19 @@
 #include "Motor.h"
 #define MOTOR_PERIOD_MS   (20)
 
+/*Motor info: State & Direction*/
+typedef struct
+{
+    tMotor_State State;
+    tMotor_Directon Direction;
+}tMotor_Info;
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+Function: Motor_Init
+Parameters: Motor from type structure tMotor
+return: void
+function job: Initialize Motor pins & first state & direction
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 void Motor_Init(tMotor Motor)
 {
     /* Initialize Motor pins*/
@@ -11,7 +24,11 @@ void Motor_Init(tMotor Motor)
     {
     case Motor_1:
         GPIO_InitPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_POSITIVE);
+
+        #ifdef MOTOR_1_BI_DIRECTION
         GPIO_InitPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_NEGATIVE);
+        #endif // MOTOR_1_BI_DIRECTION
+
         break;
 
     default:
@@ -22,21 +39,62 @@ void Motor_Init(tMotor Motor)
     Motor_Setstate(Motor,Motor_OFF,Motor_Stopped);
 }
 
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+Function: Motor_Setstate
+Parameters: Motor from type struct tMotor,State form type struct tMotor_State, Direction from type struct
+tMotor_Directon
+return: void
+function job: Set Motor state & direction
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 void Motor_Setstate(tMotor Motor,tMotor_State State,tMotor_Directon Direction)
 {
     switch(Motor)
     {
    case Motor_1:
+       /*Check if it is Clock wise*/
        if(Direction==Motor_ClockWise)
        {
-        GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_POSITIVE,State);
-        GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_NEGATIVE,0);
+           /*Check if state is on*/
+           if(State=Motor_ON)
+            {
+            GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_POSITIVE,State);
+
+            #ifdef MOTOR_1_BI_DIRECTION
+            GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_NEGATIVE,0);
+            #endif // MOTOR_1_BI_DIRECTION
+
+            }
+            /*Motor State is off*/
+           else
+            {
+                Motor_Stop(Motor);
+            }
        }
+
+       /*Check if it is Anti Clock wise*/
+       #ifdef MOTOR_1_BI_DIRECTION
        else if (Direction==Motor_AntiClockWise)
        {
+        if(State=Motor_ON)
+         {
         GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_POSITIVE,0);
         GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_NEGATIVE,State);
+         }
+         /*Motor State is off*/
+        else
+         {
+             Motor_Stop(Motor);
+         }
        }
+       #endif // MOTOR_1_BI_DIRECTION
+
+       /*Check if the Motor isn't moving any direction*/
+       else if (Direction==Motor_Stopped)
+       {
+           /*Motor State is off*/
+            Motor_Stop(Motor);
+       }
+
        else
        {
            /* No Action */
@@ -50,32 +108,59 @@ void Motor_Setstate(tMotor Motor,tMotor_State State,tMotor_Directon Direction)
     }
 }
 
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+Function: Motor_GetState
+Parameters: Motor from type structure tMotor
+return: struct tMotor_Info
+function job: Read motor state & direction
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 tMotor_Info Motor_GetState(tMotor Motor)
 {
     tMotor_Info Motor_ConfigType;
-    ConfigType.State=Motor_OFF;
-    ConfigType.Direction=Motor_Stopped;
+    /*Initialize Motor info*/
+    Motor_ConfigType.State=Motor_OFF;
+    Motor_ConfigType.Direction=Motor_Stopped;
+
     switch(Motor)
     {
     case Motor_1:
-        ConfigType.State=GPIO_ReadPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_POSITIVE);
-        if(ConfigType.State==Motor_ON)
+        /*Check for Clock wise direction*/
+        Motor_ConfigType.State=GPIO_ReadPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_POSITIVE);
+        if(Motor_ConfigType.State==Motor_ON)
             {
-               ConfigType.Direction=Motor_ClockWise;
+               Motor_ConfigType.Direction=Motor_ClockWise;
                return Motor_ConfigType;
             }
-
-        ConfigType.State=GPIO_ReadPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_NEGATIVE);
-        if(ConfigType.State==Motor_ON)
+         else
             {
-               ConfigType.Direction=Motor_AntiClockWise;
-               return Motor_ConfigType;
+                /* No Action*/
             }
 
-        if(State_ret==Motor_OFF)
+        /*Check for Clock Anti wise direction*/
+        #ifdef MOTOR_1_BI_DIRECTION
+        Motor_ConfigType.State=GPIO_ReadPortPin(MOTOR_1_PORT_CR,MOTOR_1_POLARITY_PIN_NEGATIVE);
+        if(Motor_ConfigType.State==Motor_ON)
+            {
+               Motor_ConfigType.Direction=Motor_AntiClockWise;
+               return Motor_ConfigType;
+            }
+         else
+            {
+                /* No Action*/
+            }
+
+         #endif // MOTOR_1_BI_DIRECTION
+
+        /*If Motor state is off*/
+        if(Motor_ConfigType.State==Motor_OFF)
            {
+           /*Return the initialized states at first of function*/
                return Motor_ConfigType;
            }
+        else
+            {C:\Users\Dell\Documents\Vaccum Cleaner\Motor.c
+                /* No Action*/
+            }
 
         break;
 
@@ -83,6 +168,23 @@ tMotor_Info Motor_GetState(tMotor Motor)
         /* No Action*/
         break;
     }
+    /*End of Switch*/
+}
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+Function: Motor_Stop
+Parameters: Motor from type structure tMotor
+return: struct tMotor_Info
+function job: Read motor state & direction
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+void Motor_Stop(tMotor Motor)
+{
+    GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_POSITIVE,0);
+
+    #ifdef MOTOR_1_BI_DIRECTION
+    GPIO_WritePortPin(MOTOR_1_PORT_DR,MOTOR_1_POLARITY_PIN_NEGATIVE,0);
+    #endif // MOTOR_1_BI_DIRECTION
+
 }
 
 void Motor_Update(void)
--- HAL/DC_Motor/Motor.h
+++ HAL/DC_Motor/Motor.h
@@ -1,22 +1,18 @@
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
-#define MOTOR_1_POLARITY_PIN_POSITIVE  GPIO_PIN_0
-#define MOTOR_1_POLARITY_PIN_NEGATIVE  GPIO_PIN_1
-#define MOTOR_1_PORT_DR GPIO_PORTB_DATA
-#define MOTOR_1_PORT_CR GPIO_PORTB_CONTROL
+#define MOTOR_1_BI_DIRECTION
 
+typedef enum
+{
+    Motor_1
+}tMotor;
 
 typedef struct
 {
     tMotor_State State;
     tMotor_Directon Direction;
 }tMotor_Info;
-
-typedef enum
-{
-    Motor_1
-}tMotor;
 
 typedef enum
 {
@@ -31,10 +27,11 @@ typedef enum
     Motor_Stopped
 }tMotor_Directon;
 
-
+/*Functions prototypes*/
 void Motor_Init(tMotor Motor);
 void Motor_SetState(tMotor Motor,tMotor_State State,tMotor_Directon Direction);
-void Motor_Stop(void);
+tMotor_Info Motor_GetState(tMotor Motor);
+void Motor_Stop(tMotor Motor);
 void Motor_Update(void);
 
 
