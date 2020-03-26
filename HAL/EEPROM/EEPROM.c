798d8d7248bf91b5527fa39a294082f4ac7452fa
Eslam Ehab <aboutalebrap@gmail.com>
2020-03-26 20:50:03 +0200

    Add files via upload

---
+++ HAL/EEPROM/EEPROM.c
@@ -0,0 +1,78 @@
+/* ////////////////////////////////////////////////////////////////////
+ * EEPROM.c
+ *
+ *  Author: Eslam Aboutaleb
+ /////////////////////////////////////////////////////////////////// */ 
+#include "EEPROM.h"
+
+void EEPROM_Init(void) {
+	TWI_Init(); // just initialize the TWI module inside the MC
+	SETBIT(DDRC,PC2);
+}
+
+uint8 EEPROM_Write_Byte(uint8 u8Dev_addr, uint16 u16Byte_addr,uint8 u8data) {
+	TWI_Start();
+	SETBIT(PORTC,PC2);
+	if (TWI_Get_Status() != TW_START)
+	return ERROR;
+
+	//write the device address and R/W=0 (write)
+	TWI_Write(u8Dev_addr);
+	if (TWI_Get_Status() != TW_MT_SLA_W_ACK)
+	return ERROR;
+
+	//send the required first word location address
+	TWI_Write((uint8) (u16Byte_addr >> 7));
+	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	return ERROR;
+	//send the required second word location address
+	TWI_Write((uint8) (u16Byte_addr));
+	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	return ERROR;
+	//write byte to eeprom
+	TWI_Write(u8data);
+	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	return ERROR;
+
+	TWI_Stop();
+
+	return SUCCESS;
+}
+
+uint8 EEPROM_Read_Byte(uint8 u8Dev_addr, uint16 u16Byte_addr,uint8 *u8data) {
+	TWI_Start();
+	clrBit(PORTC,PC2);
+	if (TWI_Get_Status() != TW_START)
+	return ERROR;
+
+	//write the device address and R/W=0 (write)
+	TWI_Write(u8Dev_addr);
+	if (TWI_Get_Status() != TW_MT_SLA_W_ACK)
+	return ERROR;
+
+	//send the required first word location address
+	TWI_Write((uint8) (u16Byte_addr >> 7));
+	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	return ERROR;
+	//send the required second word location address
+	TWI_Write((uint8) (u16Byte_addr));
+	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	return ERROR;
+
+	//send a repeated start
+	TWI_Start();
+	if (TWI_Get_Status() != TW_REP_START)
+	return ERROR;
+
+	//write the device address and R/W=1 (Read)
+	TWI_Write((u8Dev_addr)|1);
+	if (TWI_Get_Status() != TW_MT_SLA_R_ACK)
+	return ERROR;
+
+	*u8data = TWI_Read_With_NACK();
+	if (TWI_Get_Status() != TW_MR_DATA_NACK)
+	return ERROR;
+
+	TWI_Stop();
+	return SUCCESS;
+}
---
+++ HAL/EEPROM/EEPROM.h
@@ -0,0 +1,24 @@
+/*
+ * EEPROM.h
+ *
+ *  Author: Eslam Aboutaleb
+ */ 
+
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+#include "I2C.h"
+
+
+#define ERROR 0
+#define SUCCESS 1
+
+
+void EEPROM_Init(void);
+uint8 EEPROM_Write_Byte(uint8 u8Dev_addr, unsigned short u16Byte_addr,
+uint8 u8data);
+uint8 EEPROM_Read_Byte(uint8 u8Dev_addr, unsigned short u16Byte_addr,
+uint8 *u8data);
+
+#endif /* EEPROM_H_ */
