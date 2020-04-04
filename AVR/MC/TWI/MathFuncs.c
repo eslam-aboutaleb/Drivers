ba3b3f33f63a8d918ea48e68727226a0b8e0ce47
Eslam Ehab <aboutalebrap@gmail.com>
2020-04-04 19:44:42 +0200

    Add files via upload

---
+++ AVR/MC/TWI/MathFuncs.c
@@ -0,0 +1,13 @@
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+float64 PWR(uint16 base,uint16 exp)
+{
+	float64 Result=1;
+	while (exp != 0)
+	{
+		Result *= base;
+		exp--;
+	}
+	return Result;
+}
---
+++ AVR/MC/TWI/MathFuncs.h
@@ -0,0 +1,11 @@
+
+
+
+#ifndef MATHFUNCS_H_
+#define MATHFUNCS_H_
+
+float64 PWR(uint16 base,uint16 exp);
+
+
+
+#endif /* MATHFUNCS_H_ */
